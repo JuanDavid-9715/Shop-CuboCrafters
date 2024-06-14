8bc8252e9825ef4103f14b7a70cb59fa2ab857ac
Juan D. Medellin C <medellincalderonjuandavid@gmail.com>
2024-06-14 09:14:19 -0500

    Crud settings for promotions app

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views.categoriesView import CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
+from .views.categoryView import CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
+from .views.promotionView import PromotionListView, PromotionCreateView, PromotionUpdateView, PromotionDeleteView
 
 urlpatterns = [
+    #urls Category
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('addCategory/', CategoryCreateView.as_view(), name='addCategory'),
     path('updateCategory/<int:pk>/', CategoryUpdateView.as_view(), name='updateCategory'),
     path('deleteCategory/<int:pk>/', CategoryDeleteView.as_view(), name='deleteCategory'),
+
+    #urls Promotion
+    path('promotions/', PromotionListView.as_view(), name='promotions'),
+    path('addPromotion/', PromotionCreateView.as_view(), name='addPromotion'),
+    path('updatePromotion/<int:pk>/', PromotionUpdateView.as_view(), name='updatePromotion'),
+    path('deletePromotion/<int:pk>/', PromotionDeleteView.as_view(), name='deletePromotion'),
+
 ]
---
+++ apps/product/views/promotionView.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, reverse
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.db.models import Q
+
+from ..models.promotion import Promotion
+
+from ..forms import PromotionForm
+
+
+class PromotionListView(ListView):
+    model = Promotion
+    template_name = "product/promotion/list.html"
+
+    def get_queryset(self):
+        promotion_name = self.request.GET.get("promotion")
+
+        if promotion_name:
+            return Promotion.objects.filter(Q(name=promotion_name)).distinct()
+
+        return super().get_queryset()
+
+class PromotionCreateView(CreateView):
+    model = Promotion
+    form_class = PromotionForm
+    success_url = '/promotions/'
+    template_name = "product/promotion/create.html"
+
+class PromotionUpdateView(UpdateView):
+    model = Promotion
+    form_class = PromotionForm
+    template_name = "product/promotion/update.html"
+
+    def get_success_url(self):
+        return reverse('promotions') + f"?update=ok,{self.object.name}"
+
+class PromotionDeleteView(DeleteView):
+    model = Promotion
+    success_url = '/promotions/'
+    template_name = "product/promotion/delete.html"
