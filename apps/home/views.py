cbdd66d1d66f81c817a98d76a50cec0325bb0e7f
Juan D. Medellin C <medellincalderonjuandavid@gmail.com>
2024-06-20 20:01:52 -0500

    Home construction, entry section

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-from django.views.generic import TemplateView
+from apps.product.models.product import Product
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Product
+    paginate_by = 10
+    context_object_name = 'product_list'
     template_name = 'home/index.html'
